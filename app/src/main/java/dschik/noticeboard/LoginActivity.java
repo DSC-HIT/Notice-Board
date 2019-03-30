b049bf67f7b49f8f17fac6d47a754133c3681a40
SuranjanDaw <suranjandaw@gmail.com>
2019-03-31 01:20:38 +0530

    splash screen updated

--- app/src/main/java/dschik/noticeboard/LoginActivity.java
+++ app/src/main/java/dschik/noticeboard/LoginActivity.java
@@ -1,7 +1,9 @@
 package dschik.noticeboard;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 public class LoginActivity extends Activity {
 
     Button register;
@@ -19,11 +22,17 @@ public class LoginActivity extends Activity {
     String userrole="admin";
     TextView signin;
     private String[] userRoleString=new String[]{"admin","student"};
+    SharedPreferences sh;
+    SharedPreferences.Editor shedit;
+    private String USER_NAME = "username";
+    private String PASS_WORD = "password";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        sh = getSharedPreferences("shared",Context.MODE_PRIVATE);
+        shedit = sh.edit();
         register=(Button)findViewById(R.id.register_button);
         username=(EditText)findViewById(R.id.login_username);
         password=(EditText)findViewById(R.id.login_password);
@@ -37,10 +46,15 @@ public class LoginActivity extends Activity {
                         password.setError("Invalid password");
                     }
                     else{
-                        if(user_name.equals("heritage")&pass_word.equals("123")){
+                        if(user_name.trim().equals("heritage")&pass_word.equals("123")){
                             Intent intent=new Intent(LoginActivity.this,MainActivity.class);
                             startActivity(intent);
                             Toast.makeText(getApplicationContext(),"Login successful",Toast.LENGTH_SHORT).show();
+                            shedit.putString(USER_NAME,user_name);
+                            shedit.apply();
+                            shedit.putString(PASS_WORD,pass_word);
+                            shedit.apply();
+
                         }
                         else {
                             Toast.makeText(getApplicationContext(),"Login failed",Toast.LENGTH_SHORT).show();
--- app/src/main/java/dschik/noticeboard/SplashScreen.java
+++ app/src/main/java/dschik/noticeboard/SplashScreen.java
@@ -1,23 +1,42 @@
 package dschik.noticeboard;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 public class SplashScreen extends Activity {
+    SharedPreferences sh;
+    private String USER_NAME = "username";
+    private String PASS_WORD = "password";
+    private String DEFAULT = "null";
     private final int splash_display_length=1500;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
+        sh = getSharedPreferences("shared",Context.MODE_PRIVATE);
+
+        final String username = sh.getString(USER_NAME,DEFAULT);
+        final String password = sh.getString(PASS_WORD,DEFAULT);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent mainIntent =new Intent(SplashScreen.this,LoginActivity.class);
-                SplashScreen.this.startActivity(mainIntent);
-                SplashScreen.this.finish();
+                if(username.equals("heritage") && password.equals("123"))
+                {
+                    Intent mainIntent =new Intent(SplashScreen.this,MainActivity.class);
+                    SplashScreen.this.startActivity(mainIntent);
+                    SplashScreen.this.finish();
+                }else {
+                    Intent mainIntent = new Intent(SplashScreen.this, LoginActivity.class);
+                    SplashScreen.this.startActivity(mainIntent);
+                    SplashScreen.this.finish();
+                }
             }
         },splash_display_length);
     }
