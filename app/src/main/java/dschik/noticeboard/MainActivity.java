36c57c1484ddec9b4a8b6bc1fd09dfc051988572
SuranjanDaw <suranjandaw@gmail.com>
2019-04-01 22:15:21 +0530

    bug fixed

--- app/src/main/java/dschik/noticeboard/MainActivity.java
+++ app/src/main/java/dschik/noticeboard/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
