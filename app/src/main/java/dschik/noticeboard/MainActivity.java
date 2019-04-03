6b36d44247f6da2bd941c64de637979a0cda38e0
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2019-04-04 00:52:31 +0530

    heading changed

--- app/src/main/java/dschik/noticeboard/MainActivity.java
+++ app/src/main/java/dschik/noticeboard/MainActivity.java
@@ -99,7 +99,6 @@ public class MainActivity extends AppCompatActivity implements
             super.onBackPressed();
         }
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
