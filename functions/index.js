6eabb2eb13c0c7e69bbcebd4be7e7593c05a314b
SuranjanDaw <suranjandaw@gmail.com>
2019-04-19 20:23:55 +0530

    API testing

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 const functions = require('firebase-functions');
+const scrape = require('scrape.js');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-response.send("Hello from Firebase!");
 
+   scrape.then(noticeArray=>{  return response.send(noticeArray);})//resolved promise
+          .catch(err=>console.log(err));
 });
---
+++ functions/scrape.js
@@ -0,0 +1,30 @@
+//this module returns a promise after scraping the notice board of the website.
+
+const cheerio = require('cheerio');
+const request = require('request');
+
+function scrapeNoticeBoard(){
+    return new Promise(resolve=>{
+        request({
+            method:'GET',
+            url:'http://heritageit.edu/'
+        },(err,res)=>{
+            if(err){
+                console.log(err);
+            }else{
+                var $ = cheerio.load(res.body);
+                var notices = [];
+               $('#notice table tbody tr td table tbody ').children('tr').each((i,el)=>{
+                    var title = $(el).find('td span').eq(0).text();
+                    var a = $(el).find('a');
+                    var link = $(a).attr('href');
+                    notices[i] = {title:title,link:link};//array of objects to store notice data
+               });
+                 resolve(notices);//promise returned
+            }
+        });    
+    });
+   
+}
+
+module.exports = scrapeNoticeBoard();
