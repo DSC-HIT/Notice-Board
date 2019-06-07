fb17167575df29567c70d51e8a74e15ef0e23612
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2019-06-07 17:24:31 +0530

    onboard

--- app/src/main/java/dschik/noticeboard/OnBoard1.java
+++ app/src/main/java/dschik/noticeboard/OnBoard1.java
@@ -1,4 +1,23 @@
 package dschik.noticeboard;
 
-public class OnBoard1 {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
+public class OnBoard1 extends Activity {
+
+    private ViewPager mSlideViewpager;
+    private LinearLayout mDotLayout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.on_board1);
+
+        mSlideViewpager=(ViewPager)findViewById(R.id.slideViewPager);
+        mDotLayout=(LinearLayout) findViewById(R.id.dotsLayout);
+    }
 }
