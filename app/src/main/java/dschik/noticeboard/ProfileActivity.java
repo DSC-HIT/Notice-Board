785dca434ba093bf2b6f6a0ef3b0e5076da72fc3
SuranjanDaw <suranjandaw@gmail.com>
2019-06-10 19:16:19 +0530

    update

--- app/src/main/java/dschik/noticeboard/ProfileActivity.java
+++ app/src/main/java/dschik/noticeboard/ProfileActivity.java
@@ -5,33 +5,10 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import android.Manifest;
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.telephony.SubscriptionInfo;
-import android.telephony.SubscriptionManager;
-import android.telephony.TelephonyManager;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
-
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -52,11 +29,11 @@ public class ProfileActivity extends AppCompatActivity {
         useremail = findViewById(R.id.email_address);
         usernumber = findViewById(R.id.phone_number);
 
-        mAuth=FirebaseAuth.getInstance();
-        FirebaseUser user=mAuth.getCurrentUser();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
         assert user != null;
-        String name = sh.getString("dis_name","name");
-        String email = sh.getString("dis_email","email");
+        String name = sh.getString("dis_name", "name");
+        String email = sh.getString("dis_email", "email");
         //String number = user.getPhoneNumber();
 
         assert name != null;
@@ -75,7 +52,7 @@ public class ProfileActivity extends AppCompatActivity {
         Log.d("aa","number"+number);
         textView.setText("My Phone number is: " + number);*/
 
-    }
+}
     /*private String getMyPhoneNO() {
         TelephonyManager tMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         @SuppressLint("MissingPermission")
