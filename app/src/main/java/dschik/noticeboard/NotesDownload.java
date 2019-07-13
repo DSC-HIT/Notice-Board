112e3e65d01a17d33f43b2c72f408f322edc11ef
SuranjanDaw <suranjandaw@gmail.com>
2019-07-14 00:02:32 +0530

    notification topic set

--- app/src/main/java/dschik/noticeboard/NotesDownload.java
+++ app/src/main/java/dschik/noticeboard/NotesDownload.java
@@ -42,6 +42,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.util.ArrayList;
 
@@ -62,6 +63,7 @@ public class NotesDownload extends AppCompatActivity
     private RecyclerView.Adapter mAdapter;
     private static final String[] dept = {"CSE", "ECE", "IT", "AEIE", "BT", "EE", "ChE", "ME", "CE"};
     private static final String[] year = {"1","2","3","4"};
+    private static final String topic = "Notes";
     private boolean flag0 = false;
     private boolean flag1 = false;
     private String currentDept;
@@ -179,6 +181,7 @@ public class NotesDownload extends AppCompatActivity
             }
         });
         refreshContent();// get the content from db according to present state
+        FirebaseMessaging.getInstance().subscribeToTopic("pushNotifications_"+topic+"_"+currentDept+"_"+currentYear);
 
     }
 
@@ -225,7 +228,7 @@ public class NotesDownload extends AppCompatActivity
 
     private void searchContent(String newText,String dept, String year) {
 
-        Query myquery = dbref.child("test_data").child("Notes").child(dept).child(year).orderByChild("lable").startAt(newText).endAt(newText+"\uf8ff");
+        Query myquery = dbref.child("test_data").child(topic).child(dept).child(year).orderByChild("lable").startAt(newText).endAt(newText+"\uf8ff");
         myquery.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
@@ -275,7 +278,7 @@ public class NotesDownload extends AppCompatActivity
 
 
     private void getContent(String dept, String year) {
-        dbref.child("test_data").child("Notes").child(dept).child(year).addChildEventListener(new ChildEventListener() {
+        dbref.child("test_data").child(topic).child(dept).child(year).addChildEventListener(new ChildEventListener() {
 
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, String s) {
--- app/src/main/java/dschik/noticeboard/Notification.java
+++ app/src/main/java/dschik/noticeboard/Notification.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 
@@ -17,6 +18,7 @@ import com.google.firebase.messaging.RemoteMessage;
 public class Notification extends FirebaseMessagingService {
 
     private static final String TAG = "FirebaseMessagingServce";
+    String CHANNEL_ID = "1";
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
@@ -30,6 +32,7 @@ public class Notification extends FirebaseMessagingService {
             notificationBody = remoteMessage.getNotification().getBody();
         }
 
+        showLog("yes");
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
         sendNotification(notificationTitle, notificationBody);
@@ -38,13 +41,14 @@ public class Notification extends FirebaseMessagingService {
 
     private void sendNotification(String notificationTitle, String notificationBody) {
         createNotificationChannel();
+        showLog("in");
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setAutoCancel(true)   //Automatically delete the notification
                 .setSmallIcon(R.drawable.group12) //Notification icon
                 .setContentIntent(pendingIntent)
@@ -57,14 +61,17 @@ public class Notification extends FirebaseMessagingService {
 
         if (notificationManager != null) {
             notificationManager.notify(0, notificationBuilder.build());
+        } else {
+            showLog("notify manage not null");
         }
     }
 
     private void createNotificationChannel() {
-        String CHANNEL_ID = "1";
+
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            showLog("channel in");
             CharSequence name = "Cloud Channel";
             String description = "Cloud Notification Channel";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
@@ -75,7 +82,16 @@ public class Notification extends FirebaseMessagingService {
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             if (notificationManager != null) {
                 notificationManager.createNotificationChannel(channel);
+            } else {
+                showLog("not in ");
             }
+        } else {
+            showLog("not in outer");
         }
+    }
+
+    void showLog(String s)
+    {
+        Log.d("aa",s);
     }
 }
